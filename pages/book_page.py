4e8c907b21bddf2f1df3266c72d22af93c5cc2d7
nikrud <89258843+nikrud@users.noreply.github.com>
2022-08-31 14:18:01 +0300

    Small fix

--- pages/book_page.py
+++ pages/book_page.py
@@ -1,6 +1,8 @@
 from pages.base_page import BasePage
 from .locator import BookLocator
 
+
+
 class BookPage(BasePage):
 
     def open_book_accommodation(self):
@@ -17,11 +19,21 @@ class BookPage(BasePage):
         self.browser.find_element(*BookLocator.BASKET_BUTTON).click()
 
     def check_add(self):
-        assert self.is_presented(*BookLocator.CHECK_ADD)
+        assert self.is_presented(*BookLocator.CHECK_ADD), "Hotel don't added"
+
+    def check_name(self):
+        self.browser.switch_to.new_window('window')
+        booking_url = 'https://cypress-tourism-app.herokuapp.com/list_of_accommodations/'
+        self.browser.get(booking_url)
+        name_hotel = self.browser.find_element(*BookLocator.NAME_ON_BOOK_PAGE).text
+        self.browser.switch_to.window(self.browser.window_handles[0])
+        name_hotel_in_cart = self.browser.find_element(*BookLocator.NAME_ON_CART_PAGE).text
+        assert name_hotel in name_hotel_in_cart, "Names don't match"
 
     def delete_book(self):
-        self.browser.find_element(*BookLocator.DELETE_BUTTON).click()
-        assert self.is_presented(*BookLocator.EMPTY_CART)
+        button = self.browser.find_element(*BookLocator.DELETE_BUTTON)
+        button.click()
+        assert self.is_presented(*BookLocator.EMPTY_CART), 'Cart not cleared'
 
 
 
